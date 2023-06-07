f9de3ba3b864e7229f1090baa1108c526aabc3ed
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-06-07 08:45:04 +0200

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// turn-left arrow
+let arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", function () {
+  alert("Je tourne à gauche");
+});
+
+// turn-right arrow
+let arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", function () {
+  alert("je tourne à droite");
+});
